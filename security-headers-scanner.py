9ae4c3a6be12d46279b4567dde4846b15317e325
Benlahcen <benlahcen.assia@gmail.com>
2025-12-23 22:31:22 +0100

    first ligne shebang+explication

---
+++ security-headers-scanner.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+# ☝️ SHEBANG: Dit au système d'utiliser Python3 pour exécuter ce fichier
+# Permet de lancer le script avec ./security_scanner.py au lieu de python security_scanner.py
+# Pour l'activer sur Mac: chmod +x security_scanner.py
