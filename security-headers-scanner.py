552ae43ea39950a2c6cd5cbc2a3967383251dfae
Benlahcen <benlahcen.assia@gmail.com>
2025-12-23 22:35:09 +0100

    docstring explain the script in 2sec

--- security-headers-scanner.py
+++ security-headers-scanner.py
@@ -2,3 +2,7 @@
 # ☝️ SHEBANG: Dit au système d'utiliser Python3 pour exécuter ce fichier
 # Permet de lancer le script avec ./security_scanner.py au lieu de python security_scanner.py
 # Pour l'activer sur Mac: chmod +x security_scanner.py
+"""
+Security Headers Scanner
+Vérifie les headers de sécurité d'un site web
+"""
